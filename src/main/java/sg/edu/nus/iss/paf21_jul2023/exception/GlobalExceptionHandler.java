d5b0b83edd6ced555c0e2b4c722df9c8dfecf8f9
Darryl Ng <darryl1975@hotmail.com>
2023-06-21 11:25:08 +0800

    add ResourceNotFound custom exception

--- src/main/java/sg/edu/nus/iss/paf21_jul2023/exception/GlobalExceptionHandler.java
+++ src/main/java/sg/edu/nus/iss/paf21_jul2023/exception/GlobalExceptionHandler.java
@@ -5,7 +5,6 @@ import java.util.Date;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -19,6 +18,8 @@ public class GlobalExceptionHandler {
         new Date(), 
         ex.getMessage(), 
         request.getDescription(false));
+
+        LOGGER.error(ex.getMessage());
 
         return new ResponseEntity<ErrorMessage>(errMsg, HttpStatus.NOT_FOUND);
     }
